e63f7b222fee8adcedd6938fc2cbd78c34bc0732
Satya <59178285+kosurukompany@users.noreply.github.com>
2020-03-26 15:41:15 -0500

    Application primary test class | Satya Kosuru

--- myretail-restapi-service/src/test/java/com/target/casestudy/test/MyretailRestapiServiceApplicationTests.java
+++ myretail-restapi-service/src/test/java/com/target/casestudy/test/MyretailRestapiServiceApplicationTests.java
@@ -3,6 +3,11 @@ package com.target.casestudy.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author Satya Kosuru
+ *
+ */
+
 @SpringBootTest
 class MyretailRestapiServiceApplicationTests {
 
